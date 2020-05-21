51daf1563ecf301b56f61aeb1ae022159d09b20e
prateek guglani <prateekguglani1998@gmail.com>
2020-05-21 14:44:55 +0530

    first commit :directory structure created

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = 8000;
+
+app.listen(port,function(err)
+{
+    if(err)
+    {
+        console.error(`Error in running the server : ${err}`);
+    }
+    console.log(`Server running on port : ${port}`);
+})
